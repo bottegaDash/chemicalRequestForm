354ab15e265cae6007f77f0d6bee54b761bc126d
David Ash <bottegadash@gmail.com>
2018-12-01 03:18:23 -0500

    hookup to react

---
+++ app/assets/javascripts/components/_request_form.jsx
@@ -0,0 +1,13 @@
+class RequestForm extends React.Component {
+  constructor(reqForm) {
+    super(reqForm);
+  }
+
+  render() {
+    return (
+      <div>
+        shfwegiorbguf
+      </div>
+    );
+  }
+}
