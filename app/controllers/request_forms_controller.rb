bf6c8204e72d9bccfc5ca237af79151da1ba8f10
David Ash <bottegadash@gmail.com>
2018-12-02 21:24:41 -0500

    Added ability to prevent modification of chemicals

--- app/controllers/request_forms_controller.rb
+++ app/controllers/request_forms_controller.rb
@@ -15,7 +15,11 @@ class RequestFormsController < ApplicationController
   # GET /request_forms/new
   def new
     @request_form = RequestForm.new
-    3.times { @request_form.carts.build  }
+    @chemical = Chemical.all
+    @chemical.each do |chem|
+      a = @request_form.carts.build
+      a.chemical=chem.name
+    end
   end
 
   # GET /request_forms/1/edit
@@ -26,8 +30,13 @@ class RequestFormsController < ApplicationController
   # POST /request_forms.json
   def create
     @request_form = RequestForm.new(request_form_params)
-
     respond_to do |format|
+      chem = Chemical.all
+      i = 0
+      @request_form.carts.each do |cart|
+        cart.chemical = chem[i].name
+        i = i + 1
+      end
       if @request_form.save
         format.html { redirect_to @request_form, notice: 'Request form was successfully created.' }
         format.json { render :show, status: :created, location: @request_form }
@@ -41,7 +50,14 @@ class RequestFormsController < ApplicationController
   # PATCH/PUT /request_forms/1
   # PATCH/PUT /request_forms/1.json
   def update
+    @request_form = RequestForm.find(params[:id])
     respond_to do |format|
+      chem = Chemical.all
+      i = 0
+      @request_form.carts.each do |cart|
+        cart.chemical = chem[i].name
+        i = i + 1
+      end
       if @request_form.update(request_form_params)
         format.html { redirect_to @request_form, notice: 'Request form was successfully updated.' }
         format.json { render :show, status: :ok, location: @request_form }
@@ -55,6 +71,7 @@ class RequestFormsController < ApplicationController
   # DELETE /request_forms/1
   # DELETE /request_forms/1.json
   def destroy
+    @request_form = RequestForm.find(params[:id])
     @request_form.destroy
     respond_to do |format|
       format.html { redirect_to request_forms_url, notice: 'Request form was successfully destroyed.' }
@@ -71,6 +88,6 @@ class RequestFormsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def request_form_params
       params.require(:request_form).permit(:emergency, :additional_comments,
-                                           carts_attributes: [:chemical, :count])
+                                           carts_attributes: [:id, :chemical, :count])
     end
 end
